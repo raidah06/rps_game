788e364c6c65df67623469020e4124bc69861f64
raid <muhammadraid2006@gmail.com>
2024-05-09 18:09:42 +0800

    added a loop that repeats the game

--- main.js
+++ main.js
@@ -14,18 +14,30 @@ function getHumanChoice(){
     let Choice=humanChoice.toLowerCase();
     return Choice;
 }
-function playRound(playerSelection,computerSelection){
-    if (playerSelection === computerSelection) {
-        return "it's a tie replay this round";
-      } else if ((playerSelection === "rock" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "scissors")||(playerSelection === "scissors" && computerSelection === "rock")) {
-        HumanScore +=1;
-        return `You lose! ${playerSelection} loses to ${computerSelection}`;
-      } else if (playerSelection === "rock" && computerSelection === "scissors"||(playerSelection === "scissors" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "rock")) {
-        ComputerScore+=1;
-        return `You win! ${playerSelection} beats ${computerSelection}`;
-      }
- 
+function playGame(){
+    let HumanScore=0;
+    let ComputerScore=0;
+    function playRound(playerSelection,computerSelection){
+        if (playerSelection === computerSelection) {
+            return "it's a tie replay this round";
+        } else if ((playerSelection === "rock" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "scissors")||(playerSelection === "scissors" && computerSelection === "rock")) {
+            HumanScore +=1;
+            return `You lose! ${playerSelection} loses to ${computerSelection}`;
+        } else if (playerSelection === "rock" && computerSelection === "scissors"||(playerSelection === "scissors" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "rock")) {
+            ComputerScore+=1;
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+    
+    }
+    for(let i=0;i<5;i++){
+        console.log(playRound(getHumanChoice(),getComputerChoice()))
+    }
+    if (HumanScore>ComputerScore){
+        return "you win"
+    }else if(HumanScore<ComputerScore){
+        return "you lost"
+    }else{
+        return "it's a tie"
+    }
 }
-console.log(playRound(getHumanChoice(),getComputerChoice()));
-let HumanScore=0;
-let ComputerScore=0;
+console.log(playGame())
