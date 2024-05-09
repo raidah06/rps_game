a09f1917615b33db55365f4d2b638e0aa7359c1a
raid <muhammadraid2006@gmail.com>
2024-05-09 15:17:10 +0800

    added function to play out the round

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-function getrandomchoice(){
+function getComputerChoice(){
     let choice=Math.floor(Math.random()*3)
     if (choice==0){
         return "rock";
@@ -9,5 +9,23 @@ function getrandomchoice(){
         return "scissors";
     }
 }
-let kek=getrandomchoice()
-console.log(kek);
+function getHumanChoice(){
+    let humanChoice=prompt("Type Your Choice Out");
+    let Choice=humanChoice.toLowerCase();
+    return Choice;
+}
+function playRound(playerSelection,computerSelection){
+    if (playerSelection === computerSelection) {
+        return "it's a tie replay this round";
+      } else if ((playerSelection === "rock" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "scissors")||(playerSelection === "scissors" && computerSelection === "rock")) {
+        HumanScore +=1;
+        return `You lose! ${playerSelection} loses to ${computerSelection}`;
+      } else if (playerSelection === "rock" && computerSelection === "scissors"||(playerSelection === "scissors" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "rock")) {
+        ComputerScore+=1;
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+      }
+ 
+}
+console.log(playRound(getHumanChoice(),getComputerChoice()));
+let HumanScore=0;
+let ComputerScore=0;
